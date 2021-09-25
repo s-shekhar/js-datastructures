3fd247ff877253607d0a0fc5717a87334f5f0682
s-shekhar <shashank24@outlook.com>
2021-09-25 13:52:59 +0530

    Added SLL Remove and Reverse

--- ColtUdemyBootcamp/Linkedlist.js
+++ ColtUdemyBootcamp/Linkedlist.js
@@ -10,7 +10,7 @@ class LinkedList {
         this.head = null;
         this.tail = null;
     }
-    
+
     pprint() {
         let temp = this.head;
         let string = '';
@@ -72,68 +72,124 @@ class LinkedList {
             this.head = null;
             this.tail = null;
         }
-        else{
+        else {
             returnVal = this.head.data;
             this.head = this.head.next;
         }
         return returnVal;
     }
 
-    unshift(val){ //Insert in the beginning
+    unshift(val) { //INSERT IN THE BEGINNING 
         let node = new Node(val);
-        if(this.head === null){//No element
-                this.head = node;
-                this.tail = node;
+        if (this.head === null) {//No element
+            this.head = node;
+            this.tail = node;
         }
-        else{ //One element
+        else { //One element
             node.next = this.head;
             this.head = node;
         }
     }
 
-    get(index){
-        // let retVal = null;
+    get(index) { //GET A NODE AT GIVEN INDEX
         let temp = this.head;
-        for(let i = 0; i<index && temp !== null ; i++){
+        for (let i = 0; i < index && temp !== null; i++) {
             temp = temp.next;
         }
-        return temp? temp.data : null;
+        return temp ? temp.data : null;
     }
 
-    set(index,val){ //Change the value at a given index
+    set(index, val) { //CHANGE NODE VALUE AT GIVEN INDEX
         let i = 0;
         let temp = this.head;
-        while(i !== index && temp !== null){
+        while (i !== index && temp !== null) {
             temp = temp.next;
             i++;
         }
-        if(temp !== null) {
+        if (temp !== null) {
             temp.data = val;
         }
     }
 
-    insert(index,val){
+    insert(index, val) { //INSERT A NODE AT GIVEN INDEX
+        if (index < 0) {
+            console.log("Index value cannot be negative ");
+            return
+        }
         let node = new Node(val);
-        if(this.head === null && index === 0){
+        if (this.head === null && index === 0) {
             this.head = node;
             this.tail = node;
         }
-        else{
+        else {
             let i = 0;
             let temp = this.head;
-            while(i !== index-1 && temp !== null){ //index-1 since we need temp just before the index
+            while (i !== index - 1 && temp !== null) { //index-1 since we need temp just before the index
                 i++;
                 temp = temp.next;
             }
-            if(temp === null) return null;
-            else{
-                if(temp.next === null){  //Last node
+            if (temp === null) return null;
+            else {
+                if (temp.next === null) {  //Last node
                     this.tail = node;
                 }
                 node.next = temp.next;
                 temp.next = node;
             }
         }
+    }
+
+
+    remove(index){ //REMOVE A NODE FROM GIVEN INDEX
+        if (index < 0) {
+            console.log("Index value cannot be negative ");
+            return
+        }
+        
+        if(this.head === null) return null; // no element
+        else if(this.head === this.tail && index === 0){ // 1 element and that is to be removed
+            this.head = null;
+            this.tail = null;
+        }
+        else if(index === 0){ //Remove head
+            this.head = this.head.next;
+        }
+        else{
+            let i = 1;
+            let temp = this.head;
+
+            while(temp.next.next !== null && i !== index){
+                temp = temp.next;
+                i++;
+            }
+            if(i === index){
+                temp.next = temp.next.next;
+            }
+            else{ //Since temp.next.next === null, we need to delete tail
+                temp.next = null;
+                this.tail = temp;
+            }
+        }
+    }
+
+    reverse(){ //REVERSE A LINKED LIST IN PLACE
+        let p1 = this.head;
+        let p2 = p1.next;
+        let p3 = p2.next;
+
+        while(p3 !== null){
+            p2.next = p1;
+            if(p1 === this.head){
+                p1.next = null;
+                this.tail = p1;
+            }
+            p1 = p2;
+            p2 = p3;
+            p3 = p2.next
+            // console.log(p3)
+        }
+        p2.next = p1;
+        this.head = p2;
     }
 }
 
@@ -153,12 +209,13 @@ ll.push(7);
 // console.log("SHIFTING ",ll.shift());
 // ll.pprint();
 
-ll.insert(5,88)
-ll.insert(5,44)
+ll.insert(5, 88)
+ll.insert(5, 44)
+ll.set(2, 64)
 
 ll.pprint();
 
-ll.set(2,64)
-
+ll.reverse();
+ll.remove(2)
 
 ll.pprint();
